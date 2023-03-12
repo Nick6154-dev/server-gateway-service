31b73f64d65c70c082842ca8d2f7a3b0152b0825
Nick6154-dev <pbalsecamh5b@outlook.com>
2023-03-11 20:49:36 -0500

    Initial commit

---
+++ src/main/java/com/yps/ServerGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGatewayServiceApplication.class, args);
+    }
+
+}
